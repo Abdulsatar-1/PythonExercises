aa2b6de2811e14eb1e962a9278dad781f65a5b47
Abdulsatar Qaderzada <35701890+Satar1@users.noreply.github.com>
2019-05-16 16:04:42 +0300

    Ekat commitit

---
+++ MuotoilujenKäyttäminen.py
@@ -0,0 +1,4 @@
+
+print("Tämä on usean rivin tulostus:\n\
+Teksti on jaettu usealle riville.\n\
+Nimi:\tPetteri\nAmmatti:\tKartturi")
---
+++ NeliLaskin.py
@@ -0,0 +1,7 @@
+print("Laskin")
+num1=input("Anna ensimmäinen luku: ")
+num2=input("Anna toinen luku: ")
+num1=int(num1) #type conversion
+num2=int(num2) #type conversion
+result=num1+num2
+print("Tulos on:",result)
